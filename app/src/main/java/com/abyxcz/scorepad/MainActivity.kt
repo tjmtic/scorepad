e49cbad2975c3ad7b1719cda76d693c3e0a0f44a
TM <tjmcard@gmail.com>
2024-03-23 12:03:45 -0700

    Activity and Viewmodel - TTT slug

--- app/src/main/java/com/abyxcz/scorepad/MainActivity.kt
+++ app/src/main/java/com/abyxcz/scorepad/MainActivity.kt
@@ -3,18 +3,47 @@ package com.abyxcz.scorepad
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.itemsIndexed
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.abyxcz.scorepad.ui.theme.ScorepadTheme
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val mainViewModel: MainViewModel by viewModels()
+        var uiState: MainViewModelUiState by mutableStateOf(MainViewModelUiState.TitleScreen)
+
+        lifecycleScope.launch{
+            lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED){
+                mainViewModel.uiState.onEach{ uiState = it }.collect{}
+            }
+        }
+
         setContent {
             ScorepadTheme {
                 // A surface container using the 'background' color from the theme
@@ -22,7 +51,16 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+
+                    val state by mainViewModel.state.collectAsState()
+
+                    when(uiState){
+                        is MainViewModelUiState.TitleScreen -> { TitleScreen { mainViewModel.goToNameScreen() } }
+                        is MainViewModelUiState.NameScreen -> { NameScreen(state.nameOne, {name -> mainViewModel.updateNameOne(name)},
+                            state.nameTwo, {name -> mainViewModel.updateNameTwo(name)},
+                            { mainViewModel.goToGameScreen() }) }
+                        is MainViewModelUiState.GameScreen -> { GameScreen(state.tiles) { index -> mainViewModel.selectTile(index) } }
+                    }
                 }
             }
         }
@@ -32,9 +70,71 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "Hello edit $name!",
         modifier = modifier
     )
+}
+
+@Composable
+fun TitleScreen(onClick: () -> Unit ){
+    Column{
+        Text(text="Tic Tac Toe")
+        Button( modifier = Modifier,
+            onClick = {onClick()}, content = {
+                Text(text="Play")
+            })
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun NameScreen(value1: String, onValue1Change: (String) -> Unit, value2: String, onValue2Change: (String) -> Unit, onClick: () -> Unit){
+
+    Column{
+        TextField(value= value1, onValueChange = { onValue1Change(it)} )
+        TextField(value= value2, onValueChange = { onValue2Change(it)} )
+        Button( modifier = Modifier,
+            onClick = { onClick()}, content = {
+                Text(text="Play")
+            })
+    }
+
+
+}
+
+@Composable
+fun Tile(tile: TicTacToeTile, onClick: () -> Unit){
+
+    Box(Modifier.clickable { onClick() }) {
+        when (tile.value) {
+            1 -> {
+                Text("X")
+            }
+
+            2 -> {
+                Text("O")
+            }
+
+            else -> {
+                Text("")
+            }
+        }
+    }
+}
+
+@Composable
+fun GameScreen(board: List<TicTacToeTile>, onClick: (Int) -> Unit){
+    LazyVerticalGrid(columns= GridCells.Fixed(3)) {
+        itemsIndexed(board) { index, item ->
+            Tile(item) { onClick(index) }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewTitleScreen(){
+    TitleScreen({})
 }
 
 @Preview(showBackground = true)
@@ -43,4 +143,12 @@ fun GreetingPreview() {
     ScorepadTheme {
         Greeting("Android")
     }
+}
+
+@Preview
+@Composable
+fun GameScreenPreview(){
+    GameScreen(board = mutableListOf<TicTacToeTile>().apply{ repeat(9){ this.add(
+        TicTacToeTile()
+    )}}, onClick = {})
 }
---
+++ app/src/main/java/com/abyxcz/scorepad/MainViewModel.kt
@@ -0,0 +1,114 @@
+package com.abyxcz.scorepad
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
+import java.io.Serializable
+
+class MainViewModel: ViewModel() {
+
+    private val _uiState = MutableStateFlow<MainViewModelUiState>(MainViewModelUiState.TitleScreen)
+    val uiState: StateFlow<MainViewModelUiState> = _uiState.stateIn(
+        scope = viewModelScope, initialValue =
+        MainViewModelUiState.TitleScreen,
+        started = SharingStarted.WhileSubscribed(5000)
+    )
+
+    private val _state = MutableStateFlow<MainViewModelState>(MainViewModelState())
+    val state: StateFlow<MainViewModelState> = _state.stateIn(
+        scope = viewModelScope, initialValue =
+        MainViewModelState(),
+        started = SharingStarted.WhileSubscribed(5000)
+    )
+
+    private fun onEvent(event: MainViewModelEvent){
+        when(event){
+            is MainViewModelEvent.BeginEvent -> { _uiState.update{ MainViewModelUiState.NameScreen } }
+            is MainViewModelEvent.NameOneUpdateEvent -> { _state.update{ it.copy(nameOne = event.name) } }
+            is MainViewModelEvent.NameTwoUpdateEvent -> { _state.update{ it.copy(nameTwo = event.name) } }
+            is MainViewModelEvent.GameStartEvent -> { _uiState.update{ MainViewModelUiState.GameScreen } }
+            is MainViewModelEvent.TileUpdateEvent -> { selectTile(event.index) }
+            is MainViewModelEvent.GameFinishedEvent -> { /* show winning dialogs */ }
+        }
+    }
+
+    fun goToNameScreen(){
+        onEvent(MainViewModelEvent.BeginEvent)
+    }
+
+    fun updateNameOne(name: String){
+        onEvent(MainViewModelEvent.NameOneUpdateEvent(name))
+    }
+
+    fun updateNameTwo(name: String){
+        onEvent(MainViewModelEvent.NameTwoUpdateEvent(name))
+    }
+
+    fun goToGameScreen(){
+        onEvent(MainViewModelEvent.GameStartEvent)
+    }
+
+    fun updateTile(index: Int){
+        onEvent(MainViewModelEvent.TileUpdateEvent(index))
+    }
+
+    fun selectTile(index: Int){
+
+        _state.update {
+            if(it.tiles[index].value == 0) {
+                it.copy(
+                    turn = !it.turn,
+                    tiles = it.tiles.apply { this[index].value = if (it.turn) 1 else 2 },
+                    finished = checkBoard())
+            }
+
+            else it
+        }
+
+        if(checkBoard()){
+            finishGame()
+        }
+    }
+
+    private fun checkBoard(): Boolean{
+        //search board spaces...
+        return false
+    }
+
+    private fun finishGame(){
+        onEvent(MainViewModelEvent.GameFinishedEvent)
+    }
+
+
+}
+
+data class TicTacToeTile(var value:Int = 0)
+
+data class MainViewModelState (
+    val nameOne : String = "",
+    val nameTwo : String = "",
+    val tiles : List<TicTacToeTile> = mutableListOf<TicTacToeTile>().apply{ repeat(9){ this.add(
+        TicTacToeTile()
+    )}},
+    val turn : Boolean = true,
+    var finished: Boolean = false
+) : Serializable
+
+sealed interface MainViewModelEvent{
+    object BeginEvent: MainViewModelEvent
+    data class NameOneUpdateEvent(val name: String): MainViewModelEvent
+    data class NameTwoUpdateEvent(val name: String): MainViewModelEvent
+    object GameStartEvent: MainViewModelEvent
+    data class TileUpdateEvent(val index: Int): MainViewModelEvent
+    object GameFinishedEvent: MainViewModelEvent
+}
+
+sealed interface MainViewModelUiState{
+    object TitleScreen: MainViewModelUiState
+    object NameScreen: MainViewModelUiState
+    object GameScreen: MainViewModelUiState
+}
